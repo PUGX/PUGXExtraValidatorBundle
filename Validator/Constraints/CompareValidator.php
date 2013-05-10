b2cf90bad0a74c1fffc60d1a9c5deeffeaa4b5ce
Andrea Giuliano <giulianoand@gmail.com>
2013-05-10 17:29:44 +0200

    Modify to generic compare validator

--- Validator/Constraints/CompareValidator.php
+++ Validator/Constraints/CompareValidator.php
@@ -4,6 +4,7 @@
  * This file is part of the ExtraValidatorBundle package.
  *
  * (c) Massimiliano Arione <garakkio@gmail.com>
+ * (c) Andrea Giuliano <giulianoand@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -14,6 +15,7 @@ namespace PUGX\ExtraValidatorBundle\Validator\Constraints;
 use Symfony\Component\DependencyInjection\Container;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Component\Validator\Exception\ConstraintDefinitionException;
 
 /**
  * CompareValidator
@@ -26,12 +28,47 @@ class CompareValidator extends ConstraintValidator
     public function validate($object, Constraint $constraint)
     {
         $fromMethod = 'get' . Container::camelize($constraint->from);
-        $toMethod = 'get' . Container::camelize($constraint->to);
-        if ($object->{$fromMethod}() > $object->{$toMethod}()) {
+        $toMethod   = 'get' . Container::camelize($constraint->to);
+        $compareMethod = 'is' . Container::camelize($constraint->comparator);
+
+        if (!method_exists($this, $compareMethod)) {
+            throw new ConstraintDefinitionException(sprintf("'%s' is not a valid comparator.", $constraint->comparator));
+        }
+
+        if (!$this->$compareMethod($object->{$fromMethod}(), $object->{$toMethod}())) {
             $this->context->addViolation($constraint->message, array(
-                '{{ from }}' => $constraint->from,
-                '{{ to }}'   => $constraint->to,
+                '{{ from }}'        => $constraint->from,
+                '{{ to }}'          => $constraint->to,
+                '{{ comparator }}'  => $constraint->comparator,
             ));
         }
+    }
+
+    protected function isLess($from, $to)
+    {
+        return ($from < $to);
+    }
+
+    protected function isLessEqual($from, $to)
+    {
+        return ($from <= $to);
+    }
+
+    protected function isGreater($from, $to)
+    {
+        return ($from > $to);
+    }
+
+    protected function isGreaterEqual($from, $to)
+    {
+        return ($from >= $to);
+    }
+
+    protected function isEqual($from, $to) {
+        return ($from == $to);
+    }
+
+    protected function isDifferent($from, $to) {
+        return ($from != $to);
     }
 }
