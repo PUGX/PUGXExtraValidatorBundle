abfc07f2f9af78e2248991de19aaf62ce4237976
Massimiliano Arione <garakkio@gmail.com>
2013-10-30 18:32:07 +0100

    clean Extension class

--- DependencyInjection/PUGXExtraValidatorExtension.php
+++ DependencyInjection/PUGXExtraValidatorExtension.php
@@ -3,9 +3,7 @@
 namespace PUGX\ExtraValidatorBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Loader;
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -19,10 +17,5 @@ class PUGXExtraValidatorExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
-
-        //$loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        //$loader->load('services.xml');
     }
 }
